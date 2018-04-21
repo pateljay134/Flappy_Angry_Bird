0bbc7da1d3825802343f3e379051a5e1b228e20a
murtazabeans <murtazamanasawala110@gmail.com>
2018-04-21 07:00:08 -0700

    Adding initial commit for template
    Adding template classes

---
+++ Grapes.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Grapes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Grapes extends Template
+{
+    /**
+     * Act - do whatever the Grapes wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+
+}
