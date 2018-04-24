a24117f0ea12b8bbc6d4e9ec9eba8fe8c48f75cf
Harshrajsinh Rathod <Harshrajsinh96@gmail.com>
2018-04-24 14:14:09 -0700

    Started Off State

---
+++ RadioStateOff.java
@@ -0,0 +1,22 @@
+/**
+ * Write a description of class RadioStateOff here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RadioStateOff extends RadioState {
+
+    public RadioStateOff( RadioButton rs )
+    {
+       
+    }
+
+    @Override
+    public void doSwitchOn() {
+        
+    }
+    
+    public String getState() { return "OFF"; }
+
+
+}
