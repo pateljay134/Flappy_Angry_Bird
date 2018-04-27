c846e1c52f39e09eba24df3296f277e0cafb9e22
murtazabeans <murtazamanasawala110@gmail.com>
2018-04-26 19:52:39 -0700

    Adding points for apple
    Changes for apple

--- Apple.java
+++ Apple.java
@@ -8,16 +8,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Template
 {
-    ScoreManager sc= ScoreManager.getInstance();
+    ScoreManager scoreManager;
+    Store store;
+    Score sc;
+    BottomPipe bp = new BottomPipe();
+    boolean flag;
     public Apple(){
-        setImage("apple.png"); 
+       
+         setImage("apple.png"); 
          
         flag = true;
         scoreManager = ScoreManager.getInstance();
+        store = Store.getInstance();
+        sc = store.getScore();
+        
     }
+    public boolean allowed() { return flag; }
     public void increasePoints(int score){
-        // checking hardcoded values
-        scoreManager.addScore(100);
+        System.out.println("Inside");
+        //FlappyWorld.updateScore(20);
+        flag = false;
+        //System.out.println(score+" <=> "+scoreManager.getScore());
+        score = scoreManager.getScore() + score;
+        scoreManager.setScore(score);
+      
+        sc.setScore(scoreManager.getScore());
     }
     
     public void decreaseSpeed(){
