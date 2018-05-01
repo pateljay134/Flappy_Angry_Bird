a543db281f014e7b233165e9f72a120a6436c79d
Jay Patel <pateljay134@gmail.com>
2018-05-01 02:17:47 -0700

    Final Change adding

--- FlappyWorld.java
+++ FlappyWorld.java
@@ -28,27 +28,11 @@ public class FlappyWorld
             int  n = rand.nextInt(2) + 1;
             
             
-            if(n == 1){
                 Clouds cloud = new Clouds();
                 GreenfootImage img_cloud = cloud.getImage();
                 Random rndm_cloud = new Random();
                 myscore.addObserver(cloud);
-                int  add_cloud = rndm_cloud.nextInt( getHeight()/5) + 3;//5&5
-            }else{
-                 Clouds cloud = new Clouds();
-                GreenfootImage img_cloud = cloud.getImage();
-                Random rndm_cloud = new Random();
-                myscore.addObserver(cloud);
-                int  add_cloud = rndm_cloud.nextInt( getHeight()/5) + 3;//5&5
-            }
-
-
-            cloud = new Clouds();
-            myscore.addObserver(cloud);
-            GreenfootImage img_cloud = cloud.getImage();
-            Random rndm_cloud = new Random();
-            int  add_cloud = rndm_cloud.nextInt( getHeight()/5) + 3;//5&5
-            addObject(cloud, getWidth(), add_cloud + img_cloud.getHeight());
+                int  add_cloud = rndm_cloud.nextInt( getHeight()/5) + 3;
 
 
         
